f343c05e7cc3add62e798b20fa3721fccb9bbd5b
risticaivan <risticaivan@gmail.com>
2024-07-08 19:07:04 +0200

    build

--- src/helpers/dummyjson.tsx
+++ src/helpers/dummyjson.tsx
@@ -1,19 +1,19 @@
-// useFetchData.tsx
-import { useState, useEffect } from "react";
+// // useFetchData.tsx
+// import { useState, useEffect } from "react";
 
-const useFetchData = (URL) => {
-  const [data, setData] = useState(null);
+// const useFetchData = (URL) => {
+//   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(URL);
-      const jsonData = await response.json();
-      setData(jsonData);
-    };
-    fetchData();
-  }, [URL]); // Dependency array with URL to refetch when URL changes
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await fetch(URL);
+//       const jsonData = await response.json();
+//       setData(jsonData);
+//     };
+//     fetchData();
+//   }, [URL]); // Dependency array with URL to refetch when URL changes
 
-  return data;
-};
+//   return data;
+// };
 
-export default useFetchData;
+// export default useFetchData;
