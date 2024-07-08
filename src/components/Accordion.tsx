e91d7b0625bf90f6dcc6123eeffcbf6b66580d1a
risticaivan <risticaivan@gmail.com>
2024-07-08 18:58:08 +0200

    added animations and some more style

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,35 +1,71 @@
 import { useState, useEffect } from "react";
+import spaceInvaders from "../assets/space-invaders.svg";
 
 const URL = "https://dummyjson.com/products";
 
-interface AccordionProps {
+interface ProductListProps {
   title: string;
   description: string;
 }
 
-const Accordion: React.FC<AccordionProps> = ({ title, description }) => {
+const Product: React.FC<ProductListProps> = ({ title, description }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [readyToIvade, setReadyToIvade] = useState(false);
+  const [enterTimeoutId, setEnterTimeoutId] = useState<number | null>(null);
+
+  const handleMouseEnter = () => {
+    if (enterTimeoutId) clearTimeout(enterTimeoutId);
+    const id = window.setTimeout(() => {
+      setReadyToIvade(!readyToIvade);
+    }, 5000);
+    setEnterTimeoutId(id);
+  };
+
+  useEffect(() => {
+    return () => {
+      if (enterTimeoutId) clearTimeout(enterTimeoutId);
+    };
+  }, [enterTimeoutId]);
+
+  const handleMouseLeave = () => {
+    if (enterTimeoutId) clearTimeout(enterTimeoutId);
+    setReadyToIvade(false);
+  };
 
   return (
     <>
       <div
         className={
           !isOpen
-            ? "bg-white py-2 pr-2 pl-1"
-            : "bg-baby-blue py-2 pr-2 pl-1 border-white border-b-1 sm:border-b-2"
+            ? "bg-white py-2 pr-2 pl-1 animate-moveRight"
+            : "bg-baby-blue py-2 pr-2 pl-1 border-white border-b-2 sm:border-b-2"
         }
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         <button
-          className="flex flex-start gap-4 w-full"
+          className="flex flex-start gap-4 w-full relative"
           onClick={() => setIsOpen(!isOpen)}
         >
+          <img
+            src={spaceInvaders}
+            alt="product"
+            width={20}
+            height={20}
+            loading="lazy"
+            className={
+              readyToIvade && isOpen
+                ? "block absolute right-8 top-0 animate-invadeIconAnimation"
+                : "hidden"
+            }
+          />
           {isOpen ? (
             <svg
               width="24"
               height="24"
               viewBox="0 0 24 24"
               fill="none"
-              className="absolute"
+              className="absolute animate-fadeIn"
             >
               <rect x="5" y="11" width="14" height="2" fill="currentColor" />
             </svg>
@@ -39,22 +75,22 @@ const Accordion: React.FC<AccordionProps> = ({ title, description }) => {
               height="24"
               viewBox="0 0 24 24"
               fill="none"
-              className="absolute"
+              className="absolute delay-200"
             >
               <rect x="11" y="5" width="2" height="14" fill="currentColor" />
               <rect x="5" y="11" width="14" height="2" fill="currentColor" />
             </svg>
           )}
-          <span className="text-base font-bold ml-10">{title}</span>
+          <span className="text-base font-bold ml-10 ">{title}</span>
         </button>
         <div
-          className={`grid overflow-hidden transition-all duration-200 ease-in-out ${
+          className={`grid overflow-hidden ${
             isOpen
               ? "block grid-rows-1 opacity-100"
               : "hidden grid-rows-0 opacity-0"
           }`}
         >
-          <div className="overflow-hidden text-base font-normal ml-10">
+          <div className="overflow-hidden text-base font-normal ml-10 animate-appearFromTop">
             {description}
           </div>
         </div>
@@ -74,7 +110,7 @@ interface DataState {
   total: number;
 }
 
-const ProductList = () => {
+const Accordion = () => {
   const [data, setData] = useState<DataState>({ products: [], total: 0 });
   const [loadedProducts, setLoadedProducts] = useState<Product[]>([]);
   const [showAll, setShowAll] = useState(false);
@@ -86,7 +122,6 @@ const ProductList = () => {
       const jsonData = await response.json();
       setData({ products: jsonData.products, total: jsonData.total });
       setLoadedProducts(jsonData.products.slice(0, 4));
-      console.log(jsonData);
     };
     fetchData();
   }, []);
@@ -108,7 +143,7 @@ const ProductList = () => {
   return (
     <>
       {loadedProducts.map((product, index) => (
-        <Accordion
+        <Product
           key={index}
           title={product.title}
           description={product.description}
@@ -138,29 +173,26 @@ const ProductList = () => {
         </button>
       )}
       {showAll && (
-        <>
-          <button
-            className="bg-green-btn hover:bg-green-btn-hover text-white font-bold py-2 px-4 rounded mt-2 sm:mt-4"
-            onClick={handleShowLess}
-          >
-            <div className="flex gap-0.5 items-center">
-              <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
-                <path
-                  d="M16 4l-8 8 8 8"
-                  stroke="currentColor"
-                  strokeWidth="4"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              Show less
-            </div>
-          </button>
-          {/* <div className="absolute bottom-0">Total Products: {data.total}</div> */}
-        </>
+        <button
+          className="bg-green-btn hover:bg-green-btn-hover text-white font-bold py-2 px-4 rounded mt-2 sm:mt-4"
+          onClick={handleShowLess}
+        >
+          <div className="flex gap-0.5 items-center">
+            <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
+              <path
+                d="M16 4l-8 8 8 8"
+                stroke="currentColor"
+                strokeWidth="4"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            Show less
+          </div>
+        </button>
       )}
     </>
   );
 };
 
-export default ProductList;
+export default Accordion;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,52 @@ export default {
         'green-btn': '#82DB39',
         'green-btn-hover': '#6EBD2F',
         'baby-blue': '#E5F7F9',
-      }
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '50%': { opacity: '0.5' },
+          '100%': { opacity: '1' },
+        },
+        appearFromTop: {
+          '0%': { transform: 'translateY(-100%)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        moveRight: {
+          '0%': { transform: 'translateX(0px)' },
+          '50%': { transform: 'translateX(10px)' },
+          '100%': { transform: 'translateX(0px)' }
+        },
+        invadeIconAnimation: {
+          '0%': { transform: 'translateX(0px) translateY(0px)' },
+          '5%': { transform: 'translateX(-10px) translateY(2px)' },
+          '10%': { transform: 'translateX(-20px) translateY(4px)' },
+          '15%': { transform: 'translateX(-30px) translateY(6px)' },
+          '20%': { transform: 'translateX(-40px) translateY(8px)' },
+          '25%': { transform: 'translateX(-50px) translateY(10px)' },
+          '30%': { transform: 'translateX(-60px) translateY(8px)' },
+          '35%': { transform: 'translateX(-70px) translateY(6px)' },
+          '40%': { transform: 'translateX(-80px) translateY(4px)' },
+          '45%': { transform: 'translateX(-90px) translateY(2px)' },
+          '50%': { transform: 'translateX(-100px) translateY(0px)' },
+          '55%': { transform: 'translateX(-90px) translateY(2px)' },
+          '60%': { transform: 'translateX(-80px) translateY(4px)' },
+          '65%': { transform: 'translateX(-70px) translateY(6px)' },
+          '70%': { transform: 'translateX(-60px) translateY(8px)' },
+          '75%': { transform: 'translateX(-50px) translateY(10px)' },
+          '80%': { transform: 'translateX(-40px) translateY(8px)' },
+          '85%': { transform: 'translateX(-30px) translateY(6px)' },
+          '90%': { transform: 'translateX(-20px) translateY(4px)' },
+          '95%': { transform: 'translateX(-10px) translateY(2px)' },
+          '100%': { transform: 'translateX(0px) translateY(0px)' },
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn 300ms ease-in',
+        appearFromTop: 'appearFromTop 300ms ease-out',
+        moveRight: 'moveRight 300ms ease-in-out',
+        invadeIconAnimation: 'invadeIconAnimation 2000ms ease-in-out infinite'
+      },
     },
   },
   plugins: [],
