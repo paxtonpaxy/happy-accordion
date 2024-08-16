6e29557f631ef6605c125c21ba97f410f3f482e9
Ivan <risticaivan@gmail.com>
2024-08-16 23:33:57 +0200

    updateing the css

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -14,7 +14,7 @@ const Accordion: React.FC<AccordionProps> = ({ setMainTitle, position }) => {
 
   const data = useFetchData();
   const finalData = showAll ? data : data.slice(0, 4);
-  const mainTitle = "Veelgestelde vragen";
+  const mainTitle = "Use the button to move the Accordion";
 
   const handleLoadMore = () => {
     setIsLoading(true);
@@ -37,58 +37,60 @@ const Accordion: React.FC<AccordionProps> = ({ setMainTitle, position }) => {
   }, [mainTitle, setMainTitle]);
 
   return (
-    <div className={`accordion accordion-${position} p-4`}>
-      {finalData.map((product, index) => (
-        <Product
-          key={index}
-          title={product.title}
-          description={product.description}
-          isOpen={openItemIndex === index}
-          onClick={() => handleOpenAndClose(index)}
-        />
-      ))}
-      {!showAll && (
-        <button
-          className="bg-green-500 hover:bg-green-600 duration-300 text-white font-bold py-2 px-4 rounded mt-2 sm:mt-4 min-w-[130px]"
-          onClick={handleLoadMore}
-        >
-          {isLoading ? (
-            "Loading..."
-          ) : (
-            <div className="flex gap-1 items-center">
-              Load more
+    <div className={`accordion-wrapper accordion-wrapper-${position}`}>
+      <div className={`p-4 accordion`}>
+        {finalData.map((product, index) => (
+          <Product
+            key={index}
+            title={product.title}
+            description={product.description}
+            isOpen={openItemIndex === index}
+            onClick={() => handleOpenAndClose(index)}
+          />
+        ))}
+        {!showAll && (
+          <button
+            className="bg-green-500 hover:bg-green-600 duration-300 text-white font-bold py-2 px-4 rounded mt-2 sm:mt-4 min-w-[130px]"
+            onClick={handleLoadMore}
+          >
+            {isLoading ? (
+              "Loading..."
+            ) : (
+              <div className="flex gap-1 items-center">
+                Load more
+                <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
+                  <path
+                    d="M8 4l8 8-8 8"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+            )}
+          </button>
+        )}
+        {showAll && (
+          <button
+            className="bg-green-500 hover:bg-green-600 duration-300 text-white font-bold py-2 px-4 rounded mt-2 sm:mt-4 min-w-[130px]"
+            onClick={handleShowLess}
+          >
+            <div className="flex gap-0.5 items-center">
               <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
                 <path
-                  d="M8 4l8 8-8 8"
+                  d="M16 4l-8 8 8 8"
                   stroke="currentColor"
                   strokeWidth="4"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                 />
               </svg>
+              Show less
             </div>
-          )}
-        </button>
-      )}
-      {showAll && (
-        <button
-          className="bg-green-500 hover:bg-green-600 duration-300 text-white font-bold py-2 px-4 rounded mt-2 sm:mt-4 min-w-[130px]"
-          onClick={handleShowLess}
-        >
-          <div className="flex gap-0.5 items-center">
-            <svg width="14" height="14" viewBox="0 0 24 24" fill="none">
-              <path
-                d="M16 4l-8 8 8 8"
-                stroke="currentColor"
-                strokeWidth="4"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            Show less
-          </div>
-        </button>
-      )}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
